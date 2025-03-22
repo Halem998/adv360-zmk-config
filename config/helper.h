1444b4669ab5d6f9ae6b700387a3e033697c7826
janko <69585777+Halem998@users.noreply.github.com>
2025-03-22 20:39:42 +0100

    Update helper.h

--- config/helper.h
+++ config/helper.h
@@ -1,64 +1,40 @@
 #pragma once
 
+// Utility macros for string manipulation (if needed)
 #define ZMK_HELPER_STRINGIFY(x) #x
 
-// Preprocessor mechanism to overload macros, cf. https://stackoverflow.com/a/27051616/6114651
-#define VARGS_(_10, _9, _8, _7, _6, _5, _4, _3, _2, _1, N, ...) N
-#define VARGS(...) VARGS_(__VA_ARGS__, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
-#define CONCAT_(a, b) a##b
-#define CONCAT(a, b) CONCAT_(a, b)
-
-/* ZMK_UNICODE */
-
+// Unicode input method definitions
 #if !defined OS_UNICODE_LEAD
-    #if HOST_OS == 2
-        #define OS_UNICODE_LEAD &macro_press &kp LALT      // macOS/Windows-Alt-Codes
-    #elif HOST_OS == 1
-        #define OS_UNICODE_LEAD &macro_tap &kp LS(LC(U))   // Linux
-    #else
-        #define OS_UNICODE_LEAD &macro_tap &kp RALT &kp U  // Windows + WinCompose (default)
-    #endif
-#endif
-#if !defined OS_UNICODE_TRAIL
-    #if HOST_OS == 2
-        #define OS_UNICODE_TRAIL &macro_release &kp LALT  // macOS/Windows-Alt-Codes
-    #elif HOST_OS == 1
-        #define OS_UNICODE_TRAIL &macro_tap &kp SPACE     // Linux
-    #else
-        #define OS_UNICODE_TRAIL &macro_tap &kp RET       // Windows + WinCompose (default)
-    #endif
+    #define OS_UNICODE_LEAD &macro_press &kp LALT  // macOS Unicode input
 #endif
 
+#if !defined OS_UNICODE_TRAIL
+    #define OS_UNICODE_TRAIL &macro_release &kp LALT
+#endif
+
+// Macro for creating Unicode input sequences
 #define UC_MACRO(name, unicode_bindings) \
     / { \
         macros { \
             name: name { \
                 compatible = "zmk,behavior-macro"; \
-                wait-ms = <0>; \
-                tap-ms = <0>; \
                 #binding-cells = <0>; \
                 bindings = <OS_UNICODE_LEAD>, <&macro_tap unicode_bindings>, <OS_UNICODE_TRAIL>; \
             }; \
         }; \
     };
 
-#define UC_MODMORPH(name, uc_binding, shifted_uc_binding) \
+// Macro for creating shifted/unshifted Unicode pairs
+#define ZMK_UNICODE_PAIR(name, L0, L1, L2, L3, U0, U1, U2, U3) \
+    UC_MACRO(name ## _lower, &kp L0 &kp L1 &kp L2 &kp L3) \
+    UC_MACRO(name ## _upper, &kp U0 &kp U1 &kp U2 &kp U3) \
     / { \
         behaviors { \
             name: name { \
                 compatible = "zmk,behavior-mod-morph"; \
                 #binding-cells = <0>; \
-                bindings = <uc_binding>, <shifted_uc_binding>; \
+                bindings = <&name ## _lower>, <&name ## _upper>; \
                 mods = <(MOD_LSFT|MOD_RSFT)>; \
             }; \
         }; \
     };
-
-#define ZMK_UNICODE_SINGLE(name, L0, L1, L2, L3) \
-    UC_MACRO(name ## _lower, &kp L0 &kp L1 &kp L2 &kp L3) \
-    UC_MODMORPH(name, &name ## _lower, &none)
-
-#define ZMK_UNICODE_PAIR(name, L0, L1, L2, L3, U0, U1, U2, U3) \
-    UC_MACRO(name ## _lower, &kp L0 &kp L1 &kp L2 &kp L3) \
-    UC_MACRO(name ## _upper, &kp U0 &kp U1 &kp U2 &kp U3) \
-    UC_MODMORPH(name, &name ## _lower, &name ## _upper)
